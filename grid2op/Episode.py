95de8dcaceb49030c8a2be18a1733ad679d16bdb
Vincent Renault <vincent.renault7@gmail.com>
2019-11-27 23:38:16 +0100

    Adapted Episode class to serializable Actions and Observations spaces

--- grid2op/Episode.py
+++ grid2op/Episode.py
@@ -4,14 +4,14 @@ import os
 import numpy as np
 
 from grid2op.Exceptions import Grid2OpException
-from grid2op.Runner import Runner
+from grid2op.Utils import ActionSpace, ObservationSpace
 
 
 class Episode(object):
-    def __init__(self, runner, actions, observations, rewards, disc_lines,
-                 exec_times, params, meta, times):
+    def __init__(self, actions, observations, rewards, disc_lines,
+                 exec_times, params, meta, times, observation_space,
+                 action_space):
 
-        self.runner = runner
         self.actions = actions
         self.observations = observations
         self.rewards = rewards
@@ -20,51 +20,51 @@ class Episode(object):
         self.params = params
         self.meta = meta
         self.times = times
+        self.observation_space = observation_space
+        self.action_space = action_space
 
     def get_action(self, i):
-        return self.runner.env.action_space.from_vect(self.actions[i, :])
+        return self.action_space.from_vect(self.actions[i, :])
 
     def get_observation(self, i):
-        return self.runner.env.observation_space.from_vect(self.observations[i, :])
+        return self.observation_space.from_vect(self.observations[i, :])
 
     @classmethod
-    def fromdisk(cls, path, indx=0, grid_path=None, chronics_path=None):
+    def fromdisk(cls, agent_path, indx=0):
 
-        if path is None:
+        if agent_path is None:
             # TODO: proper exception
             raise Grid2OpException("A path to an episode should be provided")
 
-        path = os.path.abspath(path)
+        episode_path = os.path.abspath(os.path.join(agent_path, str(indx)))
+
         try:
-            with open(os.path.join(path, "_parameters.json")) as f:
+            with open(os.path.join(episode_path, "_parameters.json")) as f:
                 _parameters = json.load(fp=f)
-            with open(os.path.join(path, "episode_meta.json")) as f:
+            with open(os.path.join(episode_path, "episode_meta.json")) as f:
                 episode_meta = json.load(fp=f)
-            with open(os.path.join(path, "episode_times.json")) as f:
+            with open(os.path.join(episode_path, "episode_times.json")) as f:
                 episode_times = json.load(fp=f)
 
-            exec_times = np.load(os.path.join(path, "agent_exec_times.npy"))
-            actions = np.load(os.path.join(path, "actions.npy"))
-            observations = np.load(os.path.join(path, "observations.npy"))
+            exec_times = np.load(os.path.join(
+                episode_path, "agent_exec_times.npy"))
+            actions = np.load(os.path.join(episode_path, "actions.npy"))
+            observations = np.load(os.path.join(
+                episode_path, "observations.npy"))
             disc_lines = np.load(os.path.join(
-                path, "disc_lines_cascading_failure.npy"))
-            rewards = np.load(os.path.join(path, "rewards.npy"))
+                episode_path, "disc_lines_cascading_failure.npy"))
+            rewards = np.load(os.path.join(episode_path, "rewards.npy"))
         except FileNotFoundError as ex:
-            raise Grid2OpException(f"Result file not found \n {str(ex)}")
+            raise Grid2OpException(f"Episode file not found \n {str(ex)}")
 
-        if chronics_path is None:
-            chronics_path = episode_meta["chronics_path"]
-        if grid_path is None:
-            grid_path = episode_meta["grid_path"]
+        observation_space = ObservationSpace.from_dict(
+            os.path.join(agent_path, "dict_observation_space.json"))
+        action_space = ActionSpace.from_dict(
+            os.path.join(agent_path, "dict_action_space.json"))
 
-        runner = Runner(grid_path,
-                        chronics_path,
-                        parameters_path=os.path.join(path, "_parameters.json"))
-
-        runner.init_env()
-
-        return cls(runner, actions, observations, rewards, disc_lines,
-                   exec_times, _parameters, episode_meta, episode_times)
+        return cls(actions, observations, rewards, disc_lines,
+                   exec_times, _parameters, episode_meta, episode_times,
+                   observation_space, action_space)
 
     def todisk(self):
         pass
