61499246205c9d28ba53955796eaf4f48493b11f
Tezirg <jean.grizet@gmail.com>
2020-03-23 14:54:46 +0100

    Utils
    Fix syntax error

--- utils/make_release.py
+++ utils/make_release.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
 
     # Commit
     subprocess.run(["git", "commit", "-m", "Release v{}".format(version)])
-    subprocess.run(["git", "push", "--tag")])
+    subprocess.run(["git", "push", "--tag"])
     
     # Create new docker containers
     for vers_ in [version, "latest"]:
