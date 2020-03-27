4d35d46ab24f2659fbda9aa382bcc1a30a72d25a
Tezirg <jean.grizet@gmail.com>
2020-03-27 09:30:52 +0100

    Top level package
    Rollback top level package dataset settings export for notebooks backwards compatibility

--- grid2op/__init__.py
+++ grid2op/__init__.py
@@ -27,3 +27,13 @@ __all__ = [
 ]
 
 from grid2op.MakeEnv import make
+
+# TODO remove -- Export hardcoded datasets settings at top-level
+from grid2op.MakeEnv import CASE_14_FILE
+from grid2op.MakeEnv import CHRONICS_FODLER
+from grid2op.MakeEnv import CHRONICS_MLUTIEPISODE
+from grid2op.MakeEnv import NAMES_CHRONICS_TO_BACKEND
+from grid2op.MakeEnv import EXAMPLE_CHRONICSPATH
+from grid2op.MakeEnv import EXAMPLE_CASEFILE
+from grid2op.MakeEnv import L2RPN2019_CASEFILE
+from grid2op.MakeEnv import L2RPN2019_DICT_NAMES
