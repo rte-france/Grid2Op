c23d8504d93e63e19967e1f36fceeffa98c40919
Tezirg <jean.grizet@gmail.com>
2020-04-10 21:40:15 +0200

    Baseline DQN cmd helper
    Improves memo for train,eval, inspect by using env vars

--- baselines/DoubleDuelingDQN/cmd.sh
+++ baselines/DoubleDuelingDQN/cmd.sh
@@ -1,8 +1,23 @@
-./inspect_action_space.py --path_data ~/data_grid2op/rte_case14_realistic
+#!bash
 
-./train.py  --num_pre_steps 256 --num_train_steps 131072 --path_data ~/data_grid2op/rte_case14_realistic/ --name dqn-XX.0.0.0 --num_frames 4
+export DQN_NAME=dqn-X-0.0.x
+export DQN_DATA=~/data_grid2op/rte_case14_realistic
 
-./eval.py --path_data ~/data_grid2op/rte_case14_realistic/ --path_model ./dqn-XX.0.0.0 --path_logs ./logs_eval --nb_episode 5
+./inspect_action_space.py --path_data $DQN_DATA
 
-rm -rf ./logs-dqn-TopologyChangeAndDispatch.0.0.1/ && ./eval.py --path_data ~/data_grid2op/rte_case14_realistic/ --path_model ./dqn-TopologyChangeAndDispatch.0.0.1.h5 --path_logs ./logs-dqn-TopologyChangeAndDispatch.0.0.1/ --nb_episode 10 --max_steps=8000 --verbose
+./train.py \
+    --num_pre_steps 256 \
+    --num_train_steps 131072 \
+    --path_data $DQN_DATA \
+    --name $DQN_NAME \
+    --num_frames 4
 
+rm -rf ./logs-$DQN_NAME
+./eval.py \
+    --path_data $DQN_DATA \
+    --path_model ./$DQN_NAME.h5 \
+    --path_logs ./logs-$DQN_NAME \
+    --nb_episode 10 \
+    --max_steps=8000
+
+./inspect_graph.py --logdir ./logs-$DQN_NAME/
