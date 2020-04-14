902a567e422ee8a5f895efa22d6bc15599b7f49e
Tezirg <jean.grizet@gmail.com>
2020-04-14 09:09:59 +0200

    grid2op.Reward
    Minor changes and cleanup in CloseToOverFLow and GamePlay

--- grid2op/Reward/CloseToOverflowReward.py
+++ grid2op/Reward/CloseToOverflowReward.py
@@ -15,22 +15,21 @@ class CloseToOverflowReward(BaseReward):
         BaseReward.__init__(self)
         self.reward_min = 0.0
         self.reward_max = 1.0
-        self.half_n_lines = 2.0
+        self.max_overflowed = 5.0
 
     def initialize(self, env):
-        self.half_n_lines = (env.n_line // 2) * 1.0
+        pass
         
     def __call__(self,  action, env, has_error, is_done, is_illegal, is_ambiguous):
-        close_to_overflow = 0.0
-
         thermal_limits = env.backend.get_thermal_limit()
         lineflow_ratio = env.current_obs.rho
 
+        close_to_overflow = 0.0
         for ratio, limit in zip(lineflow_ratio, thermal_limits):
             # Seperate big line and small line
-            if (limit < 400.00 and ratio > 0.93) or (limit >= 400.00 and ratio > 0.97):
+            if (limit < 400.00 and ratio > 0.90) or ratio >= 0.95:
                 close_to_overflow += 1.0
 
-        close_to_overflow = np.clip(close_to_overflow, 0.0, self.half_n_lines)
-        penalty = np.interp(close_to_overflow, [0.0, self.half_n_lines], [self.reward_min, self.reward_max])
+        close_to_overflow = np.clip(close_to_overflow, 0.0, self.max_overflowed)
+        penalty = np.interp(close_to_overflow, [0.0, self.max_overflowed], [self.reward_min, self.reward_max])
         return self.reward_max - penalty
