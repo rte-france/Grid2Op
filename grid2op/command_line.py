c5bc0a1c01a047027c091af67173e0297144778d
Tezirg <jean.grizet@gmail.com>
2020-04-14 20:16:13 +0200

    grid2op binaries
    Roll back download bin for python -m running

--- grid2op/command_line.py
+++ grid2op/command_line.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from grid2op.main import main_cli as mainEntryPoint
-from grid2op.Download.download import download_cli as downloadEntryPoint
+from grid2op.download import download_cli as downloadEntryPoint
 
 def main():
     mainEntryPoint()
