4f20f38a74c538ab156a4fbb48a9eb89a2f210b1
Tezirg <jean.grizet@gmail.com>
2020-04-21 14:18:15 +0200

    Moves download.py into approriate folder

--- grid2op/command_line.py
+++ grid2op/command_line.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from grid2op.main import main_cli as mainEntryPoint
-from grid2op.download import download_cli as downloadEntryPoint
+from grid2op.Download.download import download_cli as downloadEntryPoint
 from grid2op.Episode.EpisodeReplay import replay_cli as replayEntryPoint
 
 def main():
