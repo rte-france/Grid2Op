13a30179c891843fd8b07514bd2df9bc3cb09335
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2020-04-22 11:33:12 +0200

    adding license files

--- grid2op/PlotGrid/PlotMatplot.py
+++ grid2op/PlotGrid/PlotMatplot.py
@@ -1,16 +1,21 @@
-import math
-from operator import itemgetter
+# Copyright (c) 2019-2020, RTE (https://www.rte-france.com)
+# See AUTHORS.txt
+# This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0.
+# If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
+# you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+# This file is part of Grid2Op, Grid2Op a testbed platform to model sequential decision making in power systems.
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
 from matplotlib.lines import Line2D
-from matplotlib import cm
 
 from grid2op.PlotGrid.BasePlot import BasePlot
 from grid2op.PlotGrid.LayoutUtil import layout_obs_sub_load_and_gen
 from grid2op.PlotGrid.PlotUtil import PlotUtil as pltu
-from grid2op.Exceptions import PlotError
+
 
 class PlotMatplot(BasePlot):
     def __init__(self,
