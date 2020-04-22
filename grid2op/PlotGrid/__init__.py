2d818064e7a07ffa0bb672eb058da85429102f42
Tezirg <jean.grizet@gmail.com>
2020-04-22 09:08:25 +0200

    PlotGrid make plotly export only if dep is installed

--- grid2op/PlotGrid/__init__.py
+++ grid2op/PlotGrid/__init__.py
@@ -1,9 +1,15 @@
 __all__ = [
     "BasePlot", 
-    "PlotPlotly",
     "PlotMatplot"
 ]
 
 from grid2op.PlotGrid.BasePlot import BasePlot
-from grid2op.PlotGrid.PlotPlotly import PlotPlotly
 from grid2op.PlotGrid.PlotMatplot import PlotMatplot
+
+# Contionnal export because ploty is an optionnal dep
+try:
+    from grid2op.PlotGrid.PlotPlotly import PlotPlotly
+    __all__.append("PlotPlotly")
+except:
+    # Silent fail because it is optional
+    pass
