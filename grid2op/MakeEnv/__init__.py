f9dcf4da68165cbfa000758c6782ce5e52232425
Tezirg <jean.grizet@gmail.com>
2020-04-24 16:08:09 +0200

    MakeEnv remove super deprecated exports

--- grid2op/MakeEnv/__init__.py
+++ grid2op/MakeEnv/__init__.py
@@ -6,16 +6,7 @@ __all__ = [
     "get_current_local_dir",
     "change_local_dir",
     # deprecated in v 0.8.0
-    "make_old",
-    # super deprecated
-    "CASE_14_FILE",
-    "CHRONICS_FODLER",
-    "CHRONICS_MLUTIEPISODE",
-    "NAMES_CHRONICS_TO_BACKEND",
-    "EXAMPLE_CHRONICSPATH",
-    "EXAMPLE_CASEFILE",
-    "L2RPN2019_CASEFILE",
-    "L2RPN2019_DICT_NAMES"
+    "make_old"
 ]
 
 from grid2op.MakeEnv.MakeOld import make_old
