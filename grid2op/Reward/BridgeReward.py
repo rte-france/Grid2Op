419e35938704efd0f0e0c97d5571564548d8be45
Tezirg <jean.grizet@gmail.com>
2020-05-01 11:59:54 +0200

    grid2op.Reward.BridgeReward
    Adds the option to allow some bridges before penalizing

--- grid2op/Reward/BridgeReward.py
+++ grid2op/Reward/BridgeReward.py
@@ -17,11 +17,12 @@ class BridgeReward(BaseReward):
     This reward computes a penalty based on how many bridges are present in the grid netwrok.
     In graph theory, a bridge is an edge that if removed will cause the graph to be disconnected.
     """
-    def __init__(self):
+    def __init__(self, min_pen_lte=0.0, max_pen_gte=1.0):
         BaseReward.__init__(self)
         self.reward_min = dt_float(0.0)
         self.reward_max = dt_float(1.0)
-        self.max_penalty_at = dt_float(2.0)
+        self.min_pen_lte = dt_float(min_pen_lte)
+        self.max_pen_gte = dt_float(max_pen_gte)
 
     def __call__(self, action, env, has_error,
                  is_done, is_illegal, is_ambiguous):
@@ -69,8 +70,11 @@ class BridgeReward(BaseReward):
         # Find the bridges
         n_bridges = dt_float(len(list(nx.bridges(G))))
 
-        n_bridges = min(n_bridges, self.max_penalty_at)
+        # Clip to min penalty
+        n_bridges = max(n_bridges, self.min_pen_lte)
+        # Clip to max penalty
+        n_bridges = min(n_bridges, self.max_pen_gte)
         r = np.interp(n_bridges,
-                      [dt_float(0.0), self.max_penalty_at],
+                      [self.min_pen_lte, self.max_pen_gte],
                       [self.reward_max, self.reward_min])
         return dt_float(r)
