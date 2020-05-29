4abf303c7ea57e5e1fae79a3e00f95466af0dab8
Tezirg <jean.grizet@gmail.com>
2020-05-29 11:27:08 +0200

    Rewards: Adjust Overflow thresholds and adds a LinesCapacity based reward

--- grid2op/Reward/LinesCapacityReward.py
+++ grid2op/Reward/LinesCapacityReward.py
@@ -5,42 +5,33 @@
 # you can obtain one at http://mozilla.org/MPL/2.0/.
 # SPDX-License-Identifier: MPL-2.0
 # This file is part of Grid2Op, Grid2Op a testbed platform to model sequential decision making in power systems.
+
 import numpy as np
 from grid2op.Reward.BaseReward import BaseReward
 from grid2op.dtypes import dt_float
 
-class CloseToOverflowReward(BaseReward):
+class LinesCapacityReward(BaseReward):
     """
-    This reward finds all lines close to overflowing.
-    Returns max reward when there is no overflow, min reward if more than one line is close to overflow
-    and the mean between max and min reward if one line is close to overflow
+    Reward based on lines capacity usage
+    Returns max reward if no current is flowing in the lines
+    Returns min reward if all lines are used at max capacity
     """
     def __init__(self):
         BaseReward.__init__(self)
         self.reward_min = dt_float(0.0)
         self.reward_max = dt_float(1.0)
-        self.max_overflowed = dt_float(5.0)
 
     def initialize(self, env):
         pass
-        
+
     def __call__(self,  action, env, has_error,
                  is_done, is_illegal, is_ambiguous):
         if has_error or is_illegal or is_ambiguous:
             return self.reward_min
 
-        thermal_limits = env.backend.get_thermal_limit()
-        lineflow_ratio = env.current_obs.rho
-
-        close_to_overflow = dt_float(0.0)
-        for ratio, limit in zip(lineflow_ratio, thermal_limits):
-            # Seperate big line and small line
-            if (limit < 400.00 and ratio >= 0.95) or ratio >= 0.975:
-                close_to_overflow += dt_float(1.0)
-
-        close_to_overflow = np.clip(close_to_overflow,
-                                    dt_float(0.0), self.max_overflowed)
-        reward = np.interp(close_to_overflow,
-                           [dt_float(0.0), self.max_overflowed],
+        usage = np.sum(env.current_obs.rho)
+        usage = np.clip(usage, 0.0, float(env.n_line))
+        reward = np.interp(usage,
+                           [dt_float(0.0), float(env.n_line)],
                            [self.reward_max, self.reward_min])
         return reward
