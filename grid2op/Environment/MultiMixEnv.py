0a6ffb475dc1f5cb332c3f5057f6a3def8f26bca
Tezirg <jean.grizet@gmail.com>
2020-06-05 16:28:25 +0200

    MultiMixEnv better handles random seeding. Remove duplicate gettter methods, since we use pythonic attribute forwaring

--- grid2op/Environment/MultiMixEnv.py
+++ grid2op/Environment/MultiMixEnv.py
@@ -28,6 +28,7 @@ class MultiMixEnvironment(GridObjects, RandomObject):
         RandomObject.__init__(self)
 
         self.current_env = None
+        self.env_index = None
         self._envs = []
 
         # Inline import to prevent cyclical import
@@ -47,8 +48,9 @@ class MultiMixEnvironment(GridObjects, RandomObject):
         if len(self._envs) == 0:
             err_msg = "MultiMix envs_dir did not contain any valid env"
             raise EnvError(err_msg)
-            
-        self.current_env = self._envs[0]
+
+        self.env_index = 0
+        self.current_env = self._envs[self.env_index]
         # Make sure GridObject class attributes are set from first env
         # Shouldbe fine since the grid is the same for all envs
         self.__class__ = self.init_grid(self.current_env)
@@ -56,9 +58,13 @@ class MultiMixEnvironment(GridObjects, RandomObject):
     def __getattr__(self, name):
         return getattr(self.current_env, name)
 
-    def reset(self):
-        rnd_env_idx = np.random.randint(len(self._envs))
-        self.current_env = self._envs[rnd_env_idx]
+    def reset(self, random=False):
+        if random:
+            self.env_index = self.space_prng.randint(len(self._envs))
+        else:
+            self.env_index = (self.env_index + 1) % len(self._envs)
+        
+        self.current_env = self._envs[self.env_index]
         self.current_env.reset()
         return self.get_obs()
 
@@ -86,7 +92,14 @@ class MultiMixEnvironment(GridObjects, RandomObject):
                                    "Make sure it can be converted to a" \
                                    "numpy 64 integer.")
 
-        return super().seed(seed)
+        s = super().seed(seed)
+        seeds = [s]
+        max_dt_int = np.iinfo(dt_int).max
+        for env in self._envs:
+            env_seed = self.space_prng.randint(max_dt_int)
+            env_seeds = env.seed(env_seed)
+            seeds.append(env_seeds)
+        return seeds
 
     def deactivate_forecast(self):
         for e in self._envs:
@@ -103,61 +116,6 @@ class MultiMixEnvironment(GridObjects, RandomObject):
         """
         for e in self._envs:
             e.set_thermal_limit(thermal_limit)
-
-    def get_obs(self):
-        """
-        Return the observations of the current environment made by 
-        the :class:`grid2op.BaseAgent.BaseAgent`.
-
-        Returns
-        -------
-        res: :class:`grid2op.Observation.Observation`
-            The current BaseObservation given to the 
-            :class:`grid2op.BaseAgent.BaseAgent` / bot / controler.
-        """
-        return self.current_env.get_obs()
-
-    def get_thermal_limit(self):
-        """
-        Get the current environment thermal limit in amps
-        """
-        return self.current_env.get_thermal_limit()
-
-    def step(self, action):
-        """
-        Run one timestep of the environment's dynamics. When end of
-        episode is reached, you are responsible for calling `reset()`
-        to reset this environment's state.
-        Accepts an action and returns a tuple:
-           - (observation, reward, done, info).
-
-        If the :class:`grid2op.BaseAction.BaseAction` is illegal or ambiguous,
-        the step is performed, but the action is
-        replaced with a "do nothing" action.
-
-        Parameters
-        ----------
-            action: :class:`grid2op.Action.Action`
-                an action provided by the agent that is applied 
-                on the underlying through the backend.
-
-        Returns
-        -------
-            observation: :class:`grid2op.Observation.Observation`
-                agent's observation of the current environment
-
-            reward: ``float``
-                amount of reward returned after previous action
-
-            done: ``bool``
-                whether the episode has ended, 
-                in which case further step() calls is undefined behavior
-
-            info: ``dict``
-                contains auxiliary diagnostic information
-
-        """
-        return self.current_env.step(action)
 
     def __enter__(self):
         """
@@ -194,25 +152,3 @@ class MultiMixEnvironment(GridObjects, RandomObject):
         """
         for e in self._envs:
             e.attach_layout(grid_layout)
-
-    def fast_forward_chronics(self, nb_timestep):
-        """
-        This method allows you to skip some time step at the 
-        beginning of the chronics.
-
-        This is usefull at the beginning of the training, 
-        if you want your agent to learn on more diverse scenarios.
-        Indeed, the data provided in the chronics usually starts 
-        always at the same date time (for example Jan 1st at 00:00). 
-        This can lead to suboptimal exploration, as during this phase, 
-        only a few time steps are managed by the agent, 
-        so in general these few time steps will correspond to grid 
-        state around Jan 1st at 00:00.
-
-        Parameters
-        ----------
-        nb_timestep: ``int``
-            Number of time step to "fast forward"
-
-        """
-        self.current_env.fast_forward_chronics(nb_timestep)
