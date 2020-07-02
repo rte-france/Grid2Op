aa341223df202e39c18c42ea50e11122b72fe547
Tezirg <jean.grizet@gmail.com>
2020-07-02 09:45:05 +0200

    MultimixEnv: Class name concat basename from path

--- grid2op/Environment/MultiMixEnv.py
+++ grid2op/Environment/MultiMixEnv.py
@@ -68,8 +68,9 @@ class MultiMixEnvironment(GridObjects, RandomObject):
         self.current_env = self.mix_envs[self.env_index]
         # Make sure GridObject class attributes are set from first env
         # Should be fine since the grid is the same for all envs
+        multi_env_name = os.path.basename(os.path.abspath(envs_dir))
         tmp_env = copy.deepcopy(self.current_env)
-        tmp_env.env_name = "multimix"
+        tmp_env.env_name = multi_env_name
         self.__class__ = self.init_grid(tmp_env)
 
     @property
