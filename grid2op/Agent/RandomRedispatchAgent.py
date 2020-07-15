9373619d670ff2da308310850ebeff8e4878f0ab
Tezirg <jean.grizet@gmail.com>
2020-07-15 18:07:27 +0200

    Adds randomredispatchAgent to init for export and fix gen selected

--- grid2op/Agent/RandomRedispatchAgent.py
+++ grid2op/Agent/RandomRedispatchAgent.py
@@ -1,21 +1,25 @@
 from grid2op.Agent import BaseAgent
 
 class RandomRedispatchAgent(BaseAgent):
-    
-    
-    def __init__(self, action_space,n_gens_to_Redispatch=2,redispatching_increment=1):
+        
+    def __init__(self, action_space,
+                 n_gens_to_Redispatch=2,
+                 redispatching_increment=1):
         """
         Initialize agent
         :param action_space: the Grid2Op action space
-        :param n_gens_to_Redispatch: the maximum number of dispatchable generators to play with 
-        :param redispatching_increment: the redispatching MW value to play with (both Plus or Minus)
+        :param n_gens_to_Redispatch: 
+          the maximum number of dispatchable generators to play with 
+        :param redispatching_increment: 
+          the redispatching MW value to play with (both Plus or Minus)
         """
-        BaseAgent.__init__(self, action_space)
+        super().__init__(self, action_space)
         self.desired_actions = []
 
+        gen_redisp = self.action_space.gen_redispatchable
         #we create a dictionnary of redispatching actions we want to play with
-        GensToRedipsatch=[i for i in range(len(env.gen_redispatchable)) if env.gen_redispatchable[i]]
-        if(len(GensToRedipsatch)>n_gens_to_Redispatch):
+        GensToRedipsatch=[i for i in range(len(gen_redisp)) if gen_redisp[i]]
+        if len(GensToRedipsatch) > n_gens_to_Redispatch:
             GensToRedipsatch=GensToRedipsatch[0:n_gens_to_Redispatch]
         
         #action dic will have 2 actions par generator (increase or decrease by the increment) + do nothing
@@ -34,10 +38,6 @@ class RandomRedispatchAgent(BaseAgent):
         
     def act(self, observation, reward, done=False):
         """
-        By definition, all "greedy" agents are acting the same way. The only thing that can differentiate multiple
-        agents is the actions that are tested.
-        These actions are defined in the method :func:`._get_tested_action`. This :func:`.act` method implements the
-        greedy logic: take the actions that maximizes the instantaneous reward on the simulated action.
         Parameters
         ----------
         observation: :class:`grid2op.Observation.Observation`
