fix the computation of the reward and scores - bug due to conversion between MW and MWh

grid2op/Reward/EconomicReward.py
@@ -52,14 +52,14 @@ class EconomicReward(BaseReward):
                 "Impossible to use the EconomicReward reward with an environment without generators"
                 "cost. Please make sure env.redispatching_unit_commitment_availble is available."
             )
-        self.worst_cost = dt_float(np.sum(env.gen_cost_per_MW * env.gen_pmax))
+        self.worst_cost = dt_float(np.sum(env.gen_cost_per_MW * env.gen_pmax) * env.delta_time_seconds / 3600.0)
 
     def __call__(self, action, env, has_error, is_done, is_illegal, is_ambiguous):
         if has_error or is_illegal or is_ambiguous:
             res = self.reward_min
         else:
             # compute the cost of the grid
-            res = dt_float(np.sum(env.get_obs().prod_p * env.gen_cost_per_MW))
+            res = dt_float(np.sum(env.get_obs().prod_p * env.gen_cost_per_MW) * env.delta_time_seconds / 3600.0)
             # we want to minimize the cost by maximizing the reward so let's take the opposite
             res *= dt_float(-1.0)
             # to be sure it's positive, add the highest possible cost

grid2op/Reward/RedispReward.py
@@ -151,17 +151,15 @@ class RedispReward(BaseReward):
         worst_load = dt_float(np.sum(env.gen_pmax))
         # it's not the worst, but definitely an upper bound
         worst_losses = dt_float(cls_._worst_losses_ratio) * worst_load
-        worst_redisp = cls_._alpha_redisp * np.sum(
-            env.gen_pmax
-        )  # not realistic, but an upper bound
-        self.max_regret = (worst_losses + worst_redisp) * worst_marginal_cost
+        worst_redisp = cls_._alpha_redisp * np.sum(env.gen_pmax)  # not realistic, but an upper bound
+        self.max_regret = (worst_losses + worst_redisp) * worst_marginal_cost * env.delta_time_seconds / 3600.0
         self.reward_min = dt_float(cls_._min_reward)
 
         least_loads = dt_float(
             worst_load * cls_._min_load_ratio
         )  # half the capacity of the grid
         least_losses = dt_float(
-            cls_._least_losses_ratio * least_loads
+            cls_._least_losses_ratio * least_loads * env.delta_time_seconds / 3600.0
         )  # 1.5% of losses
         least_redisp = dt_float(0.0)  # lower_bound is 0
         base_marginal_cost = np.min(env.gen_cost_per_MW[env.gen_cost_per_MW > 0.0])
@@ -182,7 +180,8 @@ class RedispReward(BaseReward):
             # compute the losses
             gen_p, *_ = env.backend.generators_info()
             load_p, *_ = env.backend.loads_info()
-            losses = np.sum(gen_p) - np.sum(load_p)
+            # don't forget to convert MW to MWh !
+            losses = (np.sum(gen_p) - np.sum(load_p)) * env.delta_time_seconds / 3600.0
 
             # compute the marginal cost
             gen_activeprod_t = env._gen_activeprod_t
@@ -191,14 +190,17 @@ class RedispReward(BaseReward):
             # redispatching amount
             actual_dispatch = env._actual_dispatch
             redisp_cost = (
-                self._alpha_redisp * np.sum(np.abs(actual_dispatch)) * marginal_cost
+                self._alpha_redisp * np.sum(np.abs(actual_dispatch)) * marginal_cost * env.delta_time_seconds / 3600.0
             )
 
             # cost of losses
             losses_cost = losses * marginal_cost
 
+            # cost of storage
+            c_storage = np.sum(np.abs(env._storage_power)) * marginal_cost * env.delta_time_seconds / 3600.0
+            
             # total "regret"
-            regret = losses_cost + redisp_cost
+            regret = losses_cost + redisp_cost + c_storage
 
             # compute reward
             reward = self.max_regret - regret

grid2op/tests/test_Agent.py
@@ -109,9 +109,10 @@ class TestAgent(HelperTests):
             i, cum_reward, all_acts = self._aux_test_agent(agent)
         assert i == 31, "The powerflow diverged before step 30 for do nothing"
         expected_reward = dt_float(35140.027)
+        expected_reward = dt_float(35140.03125 / 12.)
         assert (
             np.abs(cum_reward - expected_reward, dtype=dt_float) <= self.tol_one
-        ), "The reward has not been properly computed"
+        ), f"The reward has not been properly computed {cum_reward} instead of {expected_reward}"
 
     def test_1_powerlineswitch(self):
         agent = PowerLineSwitch(self.env.action_space)
@@ -121,13 +122,12 @@ class TestAgent(HelperTests):
         assert (
             i == 31
         ), "The powerflow diverged before step 30 for powerline switch agent"
-        expected_reward = dt_float(
-            35147.55859375
-        )  # switch to using df_float in the reward, change then the results
-        expected_reward = dt_float(35147.76)
+        # switch to using df_float in the reward, change then the results
+        expected_reward = dt_float(35147.55859375)  
+        expected_reward = dt_float(35147.7685546 / 12.)
         assert (
             np.abs(cum_reward - expected_reward) <= self.tol_one
-        ), "The reward has not been properly computed"
+        ), f"The reward has not been properly computed {cum_reward} instead of {expected_reward}"
 
     def test_2_busswitch(self):
         agent = TopologyGreedy(self.env.action_space)
@@ -135,17 +135,14 @@ class TestAgent(HelperTests):
             warnings.filterwarnings("error")
             i, cum_reward, all_acts = self._aux_test_agent(agent, i_max=10)
         assert i == 11, "The powerflow diverged before step 10 for greedy agent"
-        expected_reward = dt_float(
-            12075.389
-        )  # i have more actions now, so this is not correct (though it should be..
+        # i have more actions now, so this is not correct (though it should be..
         # yet a proof that https://github.com/rte-france/Grid2Op/issues/86 is grounded
+        expected_reward = dt_float(12075.389)
         expected_reward = dt_float(12277.632)
-        # 12076.356
-        # 12076.191
-        expected_reward = dt_float(12076.356)
+        expected_reward = dt_float(12076.35644531 / 12.)
         assert (
             np.abs(cum_reward - expected_reward) <= self.tol_one
-        ), "The reward has not been properly computed"
+        ), f"The reward has not been properly computed {cum_reward} instead of {expected_reward}"
 
 
 class TestMake2Agents(HelperTests):

grid2op/tests/test_issue_187.py
@@ -24,7 +24,7 @@ class Issue187Tester(unittest.TestCase):
     """
 
     def setUp(self) -> None:
-        self.tol = 1e-6
+        self.tol = 1e-5  # otherwise issues with converting to / from float32
 
     def test_issue_187(self):
         """test the range of the reward class"""
@@ -89,7 +89,7 @@ class Issue187Tester(unittest.TestCase):
                     worst_redisp = _alpha_redisph * np.sum(
                         env.gen_pmax
                     )  # not realistic, but an upper bound
-                    max_regret = (worst_losses + worst_redisp) * worst_marginal_cost
+                    max_regret = (worst_losses + worst_redisp) * worst_marginal_cost / 12.
                     reward_min = dt_float(_min_reward)
 
                     least_loads = dt_float(
@@ -102,14 +102,14 @@ class Issue187Tester(unittest.TestCase):
                     base_marginal_cost = np.min(
                         env.gen_cost_per_MW[env.gen_cost_per_MW > 0.0]
                     )
-                    min_regret = (least_losses + least_redisp) * base_marginal_cost
+                    min_regret = (least_losses + least_redisp) * base_marginal_cost / 12.
                     reward_max = dt_float((max_regret - min_regret) / least_loads)
                     assert (
                         abs(env.reward_range[1] - reward_max) <= self.tol
-                    ), f"wrong reward max computed for {env_name}"
+                    ), f"wrong reward max computed for {env_name}: {env.reward_range[1]} vs {reward_max}"
                     assert (
                         abs(env.reward_range[0] - reward_min) <= self.tol
-                    ), f"wrong reward min computed for {env_name}"
+                    ), f"wrong reward min computed for {env_name}: {env.reward_range[0]} vs {reward_min}"
 
     def test_custom_reward_runner(self):
         """test i can generate the reward and use it in the envs"""