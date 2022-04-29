80450210403d51f8a2e99fdc19fafc700d410cd8
BDonnot <benjamin.donnot@gmail.com>
2022-04-29 14:05:38 +0200

    uprade read the docs for a more recent python version [skip ci]

--- grid2op/command_line.py
+++ grid2op/command_line.py
@@ -7,6 +7,7 @@
 # you can obtain one at http://mozilla.org/MPL/2.0/.
 # SPDX-License-Identifier: MPL-2.0
 # This file is part of Grid2Op, Grid2Op a testbed platform to model sequential decision making in power systems.
+
 import warnings
 import os
 import unittest
