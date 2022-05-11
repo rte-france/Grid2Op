91cf2bf40563b015790c0504cb1008d3ba9d7259
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2022-05-11 16:04:52 +0200

    adding opponent and maintenance in the wcci 2022 env

--- grid2op/data/l2rpn_wcci_2022_dev/config.py
+++ grid2op/data/l2rpn_wcci_2022_dev/config.py
@@ -1,10 +1,11 @@
-from grid2op.Action import PlayableAction
+from grid2op.Action import PlayableAction, PowerlineSetAction
 from grid2op.Observation import CompleteObservation
 from grid2op.Reward import RedispReward
 from grid2op.Rules import DefaultRules
 from grid2op.Chronics import Multifolder
-from grid2op.Chronics import GridStateFromFileWithForecasts
+from grid2op.Chronics import GridStateFromFileWithForecastsWithMaintenance
 from grid2op.Backend import PandaPowerBackend
+from grid2op.Opponent import GeometricOpponent, BaseActionBudget
 
 try:
     from grid2op.l2rpn_utils import ActionWCCI2022, ObservationWCCI2022
@@ -16,7 +17,37 @@ except ImportError:
     ActionWCCI2022 = PlayableAction
     ObservationWCCI2022 = CompleteObservation
     
-	
+lines_attacked = ["26_31_106",
+                  "21_22_93",
+                  "17_18_88",
+                  "4_10_162",
+                  "12_14_68",
+                  "14_32_108",
+                  "62_58_180",
+                  "62_63_160",
+                  "48_50_136",
+                  "48_53_141",
+                  "41_48_131",
+                  "39_41_121",
+                  "43_44_125",
+                  "44_45_126",
+                  "34_35_110",
+                  "54_58_154",
+                  "74_117_81",
+                  "80_79_175",
+                  "93_95_43",
+                  "88_91_33",
+                  "91_92_37",
+                  "99_105_62",
+                  "102_104_61"]
+
+opponent_attack_cooldown = 12  # 1 hour, 1 hour being 12 time steps
+opponent_attack_duration = 96  # 8 hours at maximum
+opponent_budget_per_ts = (
+    0.17  # opponent_attack_duration / opponent_attack_cooldown + epsilon
+)
+opponent_init_budget = 144.0  # no need to attack straightfully, it can attack starting at midday the first day
+
 config = {
     "backend": PandaPowerBackend,
     "action_class": ActionWCCI2022,
@@ -24,7 +55,7 @@ config = {
     "reward_class": RedispReward,
     "gamerules_class": DefaultRules,
     "chronics_class": Multifolder,
-    "grid_value_class": GridStateFromFileWithForecasts,
+    "grid_value_class": GridStateFromFileWithForecastsWithMaintenance,
     "volagecontroler_class": None,
     "names_chronics_to_grid": None,
     "thermal_limits": [ 233.4,  354.4,  792.7,  550.2,  572.2,  557.2,    8. ,  480. ,
@@ -50,5 +81,18 @@ config = {
                         441. ,  671.2,  609. ,  867.4,  494.6,  494.6,  196.7,  167. ,
                         263.4,  364.1,  359.7,  803.2,  589. ,  887.2,  615.2, 1096.8,
                         306.9,  472.6,  546.6,  370.5,  441. ,  300.3,  656.2, 1346. ,
-                        1246.5, 1196.5]
+                        1246.5, 1196.5],
+    "opponent_attack_cooldown": opponent_attack_cooldown,
+    "opponent_attack_duration": opponent_attack_duration,
+    "opponent_budget_per_ts": opponent_budget_per_ts,
+    "opponent_init_budget": opponent_init_budget,
+    "opponent_action_class": PowerlineSetAction,
+    "opponent_class": GeometricOpponent,
+    "opponent_budget_class": BaseActionBudget,
+    "kwargs_opponent": {
+        "lines_attacked": lines_attacked,
+        "attack_every_xxx_hour": 24,
+        "average_attack_duration_hour": 4,
+        "minimum_attack_duration_hour": 1,
+    },
 }
