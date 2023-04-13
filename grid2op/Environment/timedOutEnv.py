378fa3373c846f46c06bedd1468c28d6f5a973f9
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2023-04-13 11:23:56 +0200

    adding sketch of time out [skip ci]

--- grid2op/Environment/timedOutEnv.py
+++ grid2op/Environment/timedOutEnv.py
@@ -6,23 +6,46 @@
 # SPDX-License-Identifier: MPL-2.0
 # This file is part of Grid2Op, Grid2Op a testbed platform to model sequential decision making in power systems.
 
-import BaseEnv
+import time
+from math import ceil
+from grid2op.Environment.Environment import Environment
 
-class TimedOutEnvironment(BaseEnv):
+
+class TimedOutEnvironment(Environment):  # TODO heritage ou alors on met un truc de base
     """_summary_
 
     Args:
         BaseEnv (_type_): _description_
     """
-    def init(self, time_out: int):
-        """_summary_
+    def __init__(self, time_out_ms=1.0) -> None:
+        super().__init__()
+        self.__last_act_send = 0.
+        self.__last_act_received = 0.
+        self.time_out_ms = 1.  # in ms
+    
+    @staticmethod
+    def __call__(regular_env):  # TimedOutEnvironment(regular_env)
+        # TODO benjamin !
+        raise NotImplementedError()
 
-        Args:
-            time_out (int): _description_
-        """      
-        super().init()
     def step(self, action):
-        for i in range(XXX):
-            super().step(do_nothing)
-            super().step(action)
+        self.__last_act_received = time.perf_counter()
+        
+        # do the "do nothing"
+        nb_dn = int(ceil(1000. * (self.__last_act_received  - self.__last_act_send)) // int(self.time_out_ms))
         do_nothing_action = self.action_space()
+        for _ in range(nb_dn):
+            obs, reward, done, info = super().step(do_nothing_action)
+            if done:
+                return obs, reward, done, info
+        
+        # now do the action
+        obs, reward, done, info = super().step(action)
+        info["nb_do_nothing"] = nb_dn
+        self.__last_act_send = time.perf_counter()
+        return obs, reward, done, info
+
+    def reset(self):
+        res = super().reset()
+        self.__last_act_send = time.perf_counter()
+        return res
