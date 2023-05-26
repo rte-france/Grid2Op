e5e812d9db5c810afdce9ed9c219336fb58900fa
GoubetClem <clemgoub@gmail.com>
2023-05-26 09:49:27 +0200

    rulesbyarea : add in init and add copyrights

--- grid2op/Rules/rulesByArea.py
+++ grid2op/Rules/rulesByArea.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2023, RTE (https://www.rte-france.com)
+# See AUTHORS.txt
+# This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0.
+# If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
+# you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+# This file is part of Grid2Op, Grid2Op a testbed platform to model sequential decision making in power systems.
+
 import numpy as np
 from itertools import chain
 from grid2op.Rules.BaseRules import BaseRules
