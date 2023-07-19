53c80a649ab4e1646293cde88c8ba65b1c7a0efc
Laure CROCHEPIERRE <laure.crochepierre@rte-france.com>
2023-07-19 13:47:12 +0200

    add default params to assistant reward test + debug reset

--- grid2op/Reward/alertReward.py
+++ grid2op/Reward/alertReward.py
@@ -100,6 +100,7 @@ class AlertReward(BaseReward):
         
         self._i_am_simulate : bool = False
 
+    
     def initialize(self, env: "grid2op.Environment.BaseEnv"):
         self.total_time_steps = env.max_episode_duration()
         self.time_window = env.parameters.ALERT_TIME_WINDOW
@@ -115,15 +116,8 @@ class AlertReward(BaseReward):
         
         self._i_am_simulate = self.is_simulated_env(env)
         return super().initialize(env)      
-    
+
     def reset(self, env):
-        self.total_time_steps = env.max_episode_duration()
-        self.time_window = env.parameters.ALERT_TIME_WINDOW
-        self._nrows_array = self.time_window + 2
-        
-        # TODO simulate env stuff !
-        
-        # TODO vectors proper size
         self._ts_attack[:,:] = False
         self._alert_launched[:,:] = False
         self._current_id = 0
@@ -131,7 +125,7 @@ class AlertReward(BaseReward):
         
         self._i_am_simulate = self.is_simulated_env(env)
         return super().reset(env)      
-
+    
     def _update_attack(self, env):
         if env.infos["opponent_attack_line"] is None:
             # no attack at this step
