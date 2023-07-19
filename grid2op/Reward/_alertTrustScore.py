3fa19ffda2a7d9c4a8a748ee0cf5291e13e40d72
marota <antoine-marot@live.fr>
2023-07-19 12:57:37 +0200

    adding last version of alertTrustScore file from PR 489 to the last stable version - commented addon kpi code

--- grid2op/Reward/_alertTrustScore.py
+++ grid2op/Reward/_alertTrustScore.py
@@ -10,6 +10,8 @@ import numpy as np
 from grid2op.Reward import AlertReward
 from grid2op.dtypes import dt_float
 
+SURVIVOR_TIMESTEPS = 12 #nb timesteps to be considered survivor of an attack
+
 class _AlertTrustScore(AlertReward):
     """
 
@@ -23,18 +25,30 @@ class _AlertTrustScore(AlertReward):
     Implemented as a reward to make it easier to use in the context of the L2RPN competitions, this reward is based on the "alert feature" 
     where the agent is asked to send information about potential line overload issue on the grid after unpredictable powerline
     disconnection (attack of the opponent).
-    The alerts are assessed once per attack. In this scheme, this "reward" computed the assistant"cost score", which penalized the number of alerts
-    the assistant have produced during an episode. It should not be used to train an agent.
+    The alerts are assessed once per attack and cumulated over the episode. In this scheme, this "reward" computed the assistant "score",
+    which assesses how well the agent is aware of its capacities to deal with a situation during an episode.
+    It should not be used to train an agent.
     
     """
-    
+
+    #TODO
+    #Parameters to use for challenge
+    #def __init__(self,
+    #             logger=None,
+    #             reward_min_no_blackout=-1.0,
+    #             reward_min_blackout=-50,
+    #             reward_max_no_blackout=0.0,
+    #             reward_max_blackout=0.0,
+    #             reward_end_episode_bonus=0.0,
+    #             min_score=-3):
     def __init__(self,
                  logger=None,
                  reward_min_no_blackout=-1.0,
-                 reward_min_blackout=-10.0, 
+                 reward_min_blackout=-10.0,
                  reward_max_no_blackout=1.0,
                  reward_max_blackout=2.0,
-                 reward_end_episode_bonus=1.0):
+                 reward_end_episode_bonus=1.0,
+                 min_score=-1.0):
         
         super().__init__(logger,
                  reward_min_no_blackout,
@@ -43,54 +57,99 @@ class _AlertTrustScore(AlertReward):
                  reward_max_blackout,
                  reward_end_episode_bonus)
         
-        self.reward_min = dt_float(-1.0)
-        self.reward_max = dt_float(1.0)
-        self.score_min_ep = lambda k: reward_min_no_blackout * (k - 1) + reward_min_blackout
-        self.score_max_ep = lambda k: max(reward_max_no_blackout * (k - 1) + reward_max_blackout,
-                                             reward_max_no_blackout * k + reward_end_episode_bonus)
+        self.min_score = dt_float(min_score)
+        self.max_score = dt_float(1.0)
         
     def initialize(self, env):
         self._is_simul_env = self.is_simulated_env(env)
-        if self._is_simul_env:
-            return
-        self.reset(env)
+            
+        self.cumulated_reward = 0
+        #KPIs
+        self.total_nb_attacks = 0
+
+        #TODO
+        #self.total_nb_alerts = 0
+        #self.alert_attack_no_blackout = 0
+        #self.alert_attack_blackout = 0
+        #self.no_alert_attack_no_blackout = 0
+        #self.no_alert_attack_blackout = 0
+        #self.blackout_encountered = False
         return super().initialize(env)
             
     def reset(self, env):
         super().reset(env)
-        self.total_nb_attacks = 0
         self.cumulated_reward = 0
-        self.true_alert_true_attack = 0
-        self.true_alert_false_attack = 0
-        self.false_alert_true_attack = 0
-        self.false_alert_false_attack = 0
+        #KPIs
+        self.total_nb_attacks = 0
+
+        # TODO
+        #self.total_nb_alerts = 0
+        #self.alert_attack_no_blackout = 0
+        #self.alert_attack_blackout = 0
+        #self.no_alert_attack_no_blackout = 0
+        #self.no_alert_attack_blackout = 0
+        #self.blackout_encountered = False
             
     def __call__(self, action, env, has_error, is_done, is_illegal, is_ambiguous):
+
+        self.blackout_encountered = self.is_in_blackout(has_error, is_done)
+        
         score_ep = 0.
         if self._is_simul_env:
             return score_ep
             
         res = super().__call__(action, env, has_error, is_done, is_illegal, is_ambiguous)
         self.cumulated_reward += res
+
         lines_attacked = env._time_since_last_attack == 0
         self.total_nb_attacks += np.sum(lines_attacked)
-        self.true_alert_true_attack += np.sum(env._time_since_last_alert[lines_attacked]==0)
-        self.true_alert_false_attack += np.sum(env._time_since_last_alert[~lines_attacked]==0)
-        self.false_alert_true_attack += np.sum(env._time_since_last_alert[lines_attacked]!=0)
-        self.false_alert_false_attack += np.sum(env._time_since_last_alert[~lines_attacked]!=0)
+
+        # TODO
+        #lines_alerted_beforeattack = np.equal(env._time_since_last_alert, env._time_since_last_attack + 1) and lines_attacked
+        #self.total_nb_alerts += np.sum(lines_alerted_beforeattack)
             
         if not is_done:
+
+            # TODO
+            #lines_attacked_no_blackout = env._time_since_last_attack == SURVIVOR_TIMESTEPS
+#
+            #self.alert_attack_no_blackout += np.sum(lines_alerted_beforeattack[lines_attacked_no_blackout])
+            #self.no_alert_attack_no_blackout += np.sum(~lines_alerted_beforeattack[lines_attacked_no_blackout])
+            
             return score_ep
+            
         else:
-            score_min_ep = self.score_min_ep(self.total_nb_attacks)
-            score_max_ep = self.score_max_ep(self.total_nb_attacks)
-            score_ep = self._normalisation_fun(self.cumulated_reward, score_min_ep, score_max_ep)
-                
+            cm_reward_min_ep, cm_reward_max_ep = self._compute_min_max_reward(self.total_nb_attacks)
+            score_ep = self._normalisation_fun(self.cumulated_reward, cm_reward_min_ep, cm_reward_max_ep,self.min_score,self.max_score)
+
+            # TODO
+            #if self.blackout_encountered:
+            #    lines_attacked_dangerzone = (env._time_since_last_attack >= 0) * (env._time_since_last_attack < SURVIVOR_TIMESTEPS)
+            #
+            #    self.alert_attack_blackout += 1. * any(lines_alerted_beforeattack[lines_attacked_dangerzone])
+            #    self.no_alert_attack_blackout += 1. * any(~lines_alerted_beforeattack[lines_attacked_dangerzone])
+            #else :
+            #    lines_attacked_no_blackout = env._time_since_last_attack > 0
+            #
+            #    self.alert_attack_no_blackout += np.sum(lines_alerted_beforeattack[lines_attacked_no_blackout])
+            #    self.no_alert_attack_no_blackout += np.sum(~lines_alerted_beforeattack[lines_attacked_no_blackout])
+            
             return score_ep
         
     @staticmethod
-    def _normalisation_fun(score, score_min_ep, score_max_ep):
-        standardized_score = (score - score_min_ep) / (score_max_ep - score_min_ep)
-        score_ep = standardized_score * 2. - 1.
+    def _normalisation_fun(cm_reward, cm_reward_min_ep, cm_reward_max_ep,min_score,max_score):
+        standardized_score = (cm_reward - cm_reward_min_ep) / (cm_reward_max_ep - cm_reward_min_ep)
+        #score_ep = standardized_score * 2. - 1.
+        score_ep = min_score + (max_score - min_score) * standardized_score
         return score_ep
-         
+    
+    def _compute_min_max_reward(self, nb_attacks):
+        if self.blackout_encountered:
+            cm_reward_min_ep = lambda k: self.reward_min_no_blackout * (k - 1) + self.reward_min_blackout
+            cm_reward_max_ep = lambda k: self.reward_max_no_blackout * (k - 1) + self.reward_max_blackout
+        else:
+            cm_reward_min_ep = lambda k: self.reward_min_no_blackout * k
+            cm_reward_max_ep = lambda k: self.reward_max_no_blackout * k + self.reward_end_episode_bonus
+        
+        return cm_reward_min_ep(nb_attacks), cm_reward_max_ep(nb_attacks)
+        
