ff98512b8941e05b73f1f346f6667fb823bb0583
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2023-10-24 12:12:36 +0200

    adding script to test environment with grid having different file format

--- grid2op/data_test/5bus_fake_grid_format/config.py
+++ grid2op/data_test/5bus_fake_grid_format/config.py
@@ -1,7 +1,7 @@
 from grid2op.Action import TopologyAction
 from grid2op.Reward import L2RPNReward
 from grid2op.Rules import DefaultRules
-from grid2op.Chronics import Multifolder
+from grid2op.Chronics import ChangeNothing
 from grid2op.Chronics import GridStateFromFileWithForecasts
 from grid2op.Backend import PandaPowerBackend
 
@@ -11,8 +11,7 @@ config = {
     "observation_class": None,
     "reward_class": L2RPNReward,
     "gamerules_class": DefaultRules,
-    "chronics_class": Multifolder,
-    "grid_value_class": GridStateFromFileWithForecasts,
+    "chronics_class": ChangeNothing,
     "volagecontroler_class": None,
     "thermal_limits": None,
     "names_chronics_to_grid": None,
--- grid2op/tests/test_env_diff_format.py
+++ grid2op/tests/test_env_diff_format.py
@@ -9,10 +9,13 @@
 import os
 from typing import Optional, Union
 import unittest
+import warnings
 
 from grid2op.tests.helper_path_test import PATH_DATA_TEST
+import grid2op
 from grid2op.tests.aaa_test_backend_interface import AAATestBackendAPI
 from grid2op.Backend import PandaPowerBackend
+from grid2op.Runner import Runner
 FILE_FORMAT = "json_custom"
 
 
@@ -50,5 +53,46 @@ class TestBackendAPI_BackendDiffFormatTester(AAATestBackendAPI, unittest.TestCas
     def setUp(self):
         self.tests_skipped = ("test_01load_grid", "test_22_islanded_grid_make_divergence")
         return super().setUp()
+    
+
+class EnvTester_BackendDiffFormatTester(unittest.TestCase):
+    def setUp(self) -> None:
+        path_env = os.path.join(PATH_DATA_TEST, "5bus_fake_grid_format")
+        detailed_infos_for_cascading_failures = False
+        self.env = grid2op.make(path_env,
+                                backend=BackendDiffFormatTester(detailed_infos_for_cascading_failures=detailed_infos_for_cascading_failures),
+                                _add_to_name=type(self).__name__
+                                )
+        super().setUp()
+        
+    def tearDown(self) -> None:
+        self.env.close()
+        return super().tearDown()
+    
+    def test_can_make(self):
+        self.env.reset()
+        1 + 1
+    
+    def test_copy(self):
+        obs = self.env.reset()
+        env_cpy = self.env.copy()
+        obs_cpy = env_cpy.reset()
+        assert self.env.backend.supported_grid_format == ("json_custom", ) 
+        assert env_cpy.backend.supported_grid_format == ("json_custom", ) 
+        
+    def test_runner(self):
+        obs = self.env.reset()
+        env_cpy = self.env.copy()
+        runner = Runner(**self.env.get_params_for_runner())
+        runner_cpy = Runner(**env_cpy.get_params_for_runner())
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore")
+            res = runner.run(nb_episode=1, max_iter=10)
+            res_cpy = runner_cpy.run(nb_episode=1, max_iter=10)
+        for el, el_cpy in zip(res[0], res_cpy[0]):
+            assert el == el_cpy, f"{el} vs {el_cpy}"
+            
+# TODO test simulate, test simulate with copy etc.(but require an env with... simulate available)            
+            
 if __name__ == "__main__":
     unittest.main()
