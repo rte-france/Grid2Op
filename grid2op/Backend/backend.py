Debug: Fix Storage 0.0 at t=0, Add: Flex seperated in redispatch computation

grid2op/Backend/backend.py
@@ -1726,7 +1726,7 @@ class Backend(GridObjects, ABC):
         self.load_min_downtime = np.full(self.n_load, fill_value=-1, dtype=dt_int)
         self.load_cost_per_MW = np.full(self.n_load, fill_value=1.0, dtype=dt_float)
 
-        # for redispatching
+        # For flexibility
         fullpath = os.path.join(path, name)
         if not os.path.exists(fullpath):
             return

grid2op/tests/test_Action.py
@@ -118,55 +118,22 @@ def _get_action_grid_class():
         "n_busbar_per_sub": "2",
         "name_gen": ["gen_0", "gen_1", "gen_2", "gen_3", "gen_4"],
         "name_load": [
-            "load_0",
-            "load_1",
-            "load_2",
-            "load_3",
-            "load_4",
-            "load_5",
-            "load_6",
-            "load_7",
-            "load_8",
-            "load_9",
-            "load_10",
+            "load_0", "load_1", "load_2", "load_3",
+            "load_4", "load_5", "load_6", "load_7",
+            "load_8", "load_9", "load_10"
         ],
         "name_line": [
-            "line_0",
-            "line_1",
-            "line_2",
-            "line_3",
-            "line_4",
-            "line_5",
-            "line_6",
-            "line_7",
-            "line_8",
-            "line_9",
-            "line_10",
-            "line_11",
-            "line_12",
-            "line_13",
-            "line_14",
-            "line_15",
-            "line_16",
-            "line_17",
-            "line_18",
-            "line_19",
+            "line_0","line_1","line_2","line_3",
+            "line_4","line_5","line_6","line_7",
+            "line_8","line_9","line_10","line_11",
+            "line_12","line_13","line_14","line_15",
+            "line_16","line_17","line_18","line_19",
         ],
         "name_sub": [
-            "sub_0",
-            "sub_1",
-            "sub_2",
-            "sub_3",
-            "sub_4",
-            "sub_5",
-            "sub_6",
-            "sub_7",
-            "sub_8",
-            "sub_9",
-            "sub_10",
-            "sub_11",
-            "sub_12",
-            "sub_13",
+            "sub_0","sub_1","sub_2","sub_3",
+            "sub_4","sub_5","sub_6","sub_7",
+            "sub_8","sub_9","sub_10","sub_11",
+            "sub_12","sub_13",
         ],
         "name_storage": ["storage_0", "storage_1"],
         "env_name": "test_action_env",
@@ -174,95 +141,35 @@ def _get_action_grid_class():
         "load_to_subid": [1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 13],
         "gen_to_subid": [0, 1, 2, 5, 7],
         "line_or_to_subid": [
-            0,
-            0,
-            1,
-            1,
-            1,
-            2,
-            3,
-            3,
-            3,
-            4,
-            5,
-            5,
-            5,
-            6,
-            6,
-            8,
-            8,
-            9,
-            11,
-            12,
+            0,0,1,1,
+            1,2,3,3,
+            3,4,5,5,
+            5,6,6,8,
+            8,9,11,12,
         ],
         "line_ex_to_subid": [
-            1,
-            4,
-            2,
-            3,
-            4,
-            3,
-            4,
-            6,
-            8,
-            5,
-            10,
-            11,
-            12,
-            7,
-            8,
-            9,
-            13,
-            10,
-            12,
-            13,
+            1,4,2,3,
+            4,3,4,6,
+            8,5,10,11,
+            12,7,8,9,
+            13,10,12,13,
         ],
         "storage_to_subid": [1, 2],
         "load_to_sub_pos": [4, 2, 5, 4, 4, 4, 1, 1, 1, 2, 1],
         "gen_to_sub_pos": [2, 5, 3, 5, 1],
         "line_or_to_sub_pos": [
-            0,
-            1,
-            1,
-            2,
-            3,
-            1,
-            2,
-            3,
-            4,
-            3,
-            1,
-            2,
-            3,
-            1,
-            2,
-            2,
-            3,
-            0,
-            0,
-            1,
+            0,1,1,2,
+            3,1,2,3,
+            4,3,1,2,
+            3,1,2,2,
+            3,0,0,1,
         ],
         "line_ex_to_sub_pos": [
-            0,
-            0,
-            0,
-            0,
-            1,
-            1,
-            2,
-            0,
-            0,
-            0,
-            2,
-            2,
-            3,
-            0,
-            1,
-            2,
-            2,
-            0,
-            0,
-            0,
+            0,0,0,0,
+            1,1,2,0,
+            0,0,2,2,
+            3,0,1,2,
+            2,0,0,0,
         ],
         "storage_to_sub_pos": [6, 4],
         "load_pos_topo_vect": [7, 12, 20, 25, 30, 41, 43, 46, 49, 53, 56],
@@ -836,39 +743,41 @@ class TestActionBase(ABC):
         )
 
     def test_action_str(self):
-        self._skipMissingKey("set_bus")
-        self._skipMissingKey("change_bus")
+        with warnings.catch_warnings(category=DeprecationWarning, action="ignore"):
+            self._skipMissingKey("set_bus")
+            self._skipMissingKey("change_bus")
 
-        arr1 = np.array([False, False, False, True, True, True, True], dtype=dt_bool)
-        arr2 = np.array([1, 1, 2, 2], dtype=dt_int)
-        id_1 = 1
-        id_2 = 12
-        action = self.helper_action(
-            {
-                "change_bus": {"substations_id": [(id_1, arr1)]},
-                "set_bus": {"substations_id": [(id_2, arr2)]},
-            }
-        )
-        res = action.__str__()
-        act_str = (
-            "This action will:\n\t - NOT change anything to the injections"
-            "\n\t - NOT perform any redispatching action\n"
-            "\t - NOT modify any storage capacity\n"
-            "\t - NOT perform any curtailment"
-            "\n\t - NOT force any line status\n"
-            "\t - NOT switch any line status"
-            "\n\t - Change the bus of the following element(s):"
-            "\n\t \t - Switch bus of line (origin) id 4 [on substation 1]"
-            "\n\t \t - Switch bus of load id 0 [on substation 1]"
-            "\n\t \t - Switch bus of generator id 1 [on substation 1]"
-            "\n\t \t - Switch bus of storage id 0 [on substation 1]"
-            "\n\t - Set the bus of the following element(s):"
-            "\n\t \t - Assign bus 1 to line (extremity) id 18 [on substation 12]"
-            "\n\t \t - Assign bus 1 to line (origin) id 19 [on substation 12]"
-            "\n\t \t - Assign bus 2 to load id 9 [on substation 12]"
-            "\n\t \t - Assign bus 2 to line (extremity) id 12 [on substation 12]"
-        )
-        assert res == act_str
+            arr1 = np.array([False, False, False, True, True, True, True], dtype=dt_bool)
+            arr2 = np.array([1, 1, 2, 2], dtype=dt_int)
+            id_1 = 1
+            id_2 = 12
+            action = self.helper_action(
+                {
+                    "change_bus": {"substations_id": [(id_1, arr1)]},
+                    "set_bus": {"substations_id": [(id_2, arr2)]},
+                }
+            )
+            res = action.__str__()
+            act_str = (
+                "This action will:\n\t - NOT change anything to the injections"
+                "\n\t - NOT perform any redispatching action\n"
+                "\t - NOT perform any flexibility action\n"
+                "\t - NOT modify any storage capacity\n"
+                "\t - NOT perform any curtailment"
+                "\n\t - NOT force any line status\n"
+                "\t - NOT switch any line status"
+                "\n\t - Change the bus of the following element(s):"
+                "\n\t \t - Switch bus of line (origin) id 4 [on substation 1]"
+                "\n\t \t - Switch bus of load id 0 [on substation 1]"
+                "\n\t \t - Switch bus of generator id 1 [on substation 1]"
+                "\n\t \t - Switch bus of storage id 0 [on substation 1]"
+                "\n\t - Set the bus of the following element(s):"
+                "\n\t \t - Assign bus 1 to line (extremity) id 18 [on substation 12]"
+                "\n\t \t - Assign bus 1 to line (origin) id 19 [on substation 12]"
+                "\n\t \t - Assign bus 2 to load id 9 [on substation 12]"
+                "\n\t \t - Assign bus 2 to line (extremity) id 12 [on substation 12]"
+            )
+            assert res == act_str
 
     def test_to_vect(self):
         self._skipMissingKey("set_bus")
@@ -1219,89 +1128,88 @@ class TestActionBase(ABC):
         assert np.all(np.isfinite(vect_act1) == np.isfinite(vect_act2))
 
     def test_call_change_set(self):
-        self._skipMissingKey("set_bus")
-        self._skipMissingKey("change_bus")
-        self._skipMissingKey("set_line_status")
-        self._skipMissingKey("change_line_status")
-        self._skipMissingKey("injection")
+        with warnings.catch_warnings(category=DeprecationWarning, action="ignore"):
+            self._skipMissingKey("set_bus")
+            self._skipMissingKey("change_bus")
+            self._skipMissingKey("set_line_status")
+            self._skipMissingKey("change_line_status")
+            self._skipMissingKey("injection")
 
-        arr1 = np.array([False, False, False, True, True, True, True], dtype=dt_bool)
-        arr2 = np.array([1, 1, 2, 2], dtype=dt_int)
-        id_1 = 1
-        id_2 = 12
-        new_vect = np.random.randn(self.helper_action.n_load).astype(dt_int)
-        new_vect2 = np.random.randn(self.helper_action.n_load).astype(dt_int)
+            arr1 = np.array([False, False, False, True, True, True, True], dtype=dt_bool)
+            arr2 = np.array([1, 1, 2, 2], dtype=dt_int)
+            id_1 = 1
+            id_2 = 12
+            new_vect = np.random.randn(self.helper_action.n_load).astype(dt_int)
+            new_vect2 = np.random.randn(self.helper_action.n_load).astype(dt_int)
 
-        change_status_orig = np.random.randint(0, 2, self.helper_action.n_line).astype(
-            dt_bool
-        )
-        set_status_orig = np.random.randint(-1, 2, self.helper_action.n_line)
-        set_status_orig[change_status_orig] = 0
+            change_status_orig = np.random.randint(0, 2, self.helper_action.n_line).astype(
+                dt_bool
+            )
+            set_status_orig = np.random.randint(-1, 2, self.helper_action.n_line)
+            set_status_orig[change_status_orig] = 0
 
-        change_topo_vect_orig = np.random.randint(
-            0, 2, self.helper_action.dim_topo
-        ).astype(dt_bool)
-        # powerline that are set to be reconnected, can't be moved to another bus
-        change_topo_vect_orig[
-            self.helper_action.line_or_pos_topo_vect[set_status_orig == 1]
-        ] = False
-        change_topo_vect_orig[
-            self.helper_action.line_ex_pos_topo_vect[set_status_orig == 1]
-        ] = False
-        # powerline that are disconnected, can't be moved to the other bus
-        change_topo_vect_orig[
-            self.helper_action.line_or_pos_topo_vect[set_status_orig == -1]
-        ] = False
-        change_topo_vect_orig[
-            self.helper_action.line_ex_pos_topo_vect[set_status_orig == -1]
-        ] = False
+            change_topo_vect_orig = np.random.randint(
+                0, 2, self.helper_action.dim_topo
+            ).astype(dt_bool)
+            # powerline that are set to be reconnected, can't be moved to another bus
+            change_topo_vect_orig[
+                self.helper_action.line_or_pos_topo_vect[set_status_orig == 1]
+            ] = False
+            change_topo_vect_orig[
+                self.helper_action.line_ex_pos_topo_vect[set_status_orig == 1]
+            ] = False
+            # powerline that are disconnected, can't be moved to the other bus
+            change_topo_vect_orig[
+                self.helper_action.line_or_pos_topo_vect[set_status_orig == -1]
+            ] = False
+            change_topo_vect_orig[
+                self.helper_action.line_ex_pos_topo_vect[set_status_orig == -1]
+            ] = False
 
-        set_topo_vect_orig = np.random.randint(0, 3, self.helper_action.dim_topo)
-        set_topo_vect_orig[change_topo_vect_orig] = 0  # don't both change and set
-        # I need to make sure powerlines that are reconnected are indeed reconnected to a bus
-        set_topo_vect_orig[
-            self.helper_action.line_or_pos_topo_vect[set_status_orig == 1]
-        ] = 1
-        set_topo_vect_orig[
-            self.helper_action.line_ex_pos_topo_vect[set_status_orig == 1]
-        ] = 1
-        # I need to make sure powerlines that are disconnected are not assigned to a bus
-        set_topo_vect_orig[
-            self.helper_action.line_or_pos_topo_vect[set_status_orig == -1]
-        ] = 0
-        set_topo_vect_orig[
-            self.helper_action.line_ex_pos_topo_vect[set_status_orig == -1]
-        ] = 0
+            set_topo_vect_orig = np.random.randint(0, 3, self.helper_action.dim_topo)
+            set_topo_vect_orig[change_topo_vect_orig] = 0  # don't both change and set
+            # I need to make sure powerlines that are reconnected are indeed reconnected to a bus
+            set_topo_vect_orig[
+                self.helper_action.line_or_pos_topo_vect[set_status_orig == 1]
+            ] = 1
+            set_topo_vect_orig[
+                self.helper_action.line_ex_pos_topo_vect[set_status_orig == 1]
+            ] = 1
+            # I need to make sure powerlines that are disconnected are not assigned to a bus
+            set_topo_vect_orig[
+                self.helper_action.line_or_pos_topo_vect[set_status_orig == -1]
+            ] = 0
+            set_topo_vect_orig[
+                self.helper_action.line_ex_pos_topo_vect[set_status_orig == -1]
+            ] = 0
 
-        action = self.helper_action(
-            {
-                "change_bus": change_topo_vect_orig,
-                "set_bus": set_topo_vect_orig,
-                "injection": {"load_p": new_vect, "load_q": new_vect2},
-                "change_line_status": change_status_orig,
-                "set_line_status": set_status_orig,
-            }
-        )
-        (
-            dict_injection,
-            set_status,
-            change_status,
-            set_topo_vect,
-            switcth_topo_vect,
-            redispatching,
-            storage,
-            shunts,
-        ) = action()
+            action = self.helper_action(
+                {
+                    "change_bus": change_topo_vect_orig,
+                    "set_bus": set_topo_vect_orig,
+                    "injection": {"load_p": new_vect, "load_q": new_vect2},
+                    "change_line_status": change_status_orig,
+                    "set_line_status": set_status_orig,
+                }
+            )
+            (
+                dict_injection,
+                set_status,
+                change_status,
+                set_topo_vect,
+                switch_topo_vect,
+                *_
+            ) = action()
 
-        assert "load_p" in dict_injection
-        assert np.all(dict_injection["load_p"] == new_vect)
-        assert "load_q" in dict_injection
-        assert np.all(dict_injection["load_q"] == new_vect2)
+            assert "load_p" in dict_injection
+            assert np.all(dict_injection["load_p"] == new_vect)
+            assert "load_q" in dict_injection
+            assert np.all(dict_injection["load_q"] == new_vect2)
 
-        assert np.all(set_status == set_status_orig)
-        assert np.all(change_status == change_status_orig)
-        assert np.all(set_topo_vect == set_topo_vect_orig)
-        assert np.all(switcth_topo_vect == change_topo_vect_orig)
+            assert np.all(set_status == set_status_orig)
+            assert np.all(change_status == change_status_orig)
+            assert np.all(set_topo_vect == set_topo_vect_orig)
+            assert np.all(switch_topo_vect == change_topo_vect_orig)
 
     def test_get_topological_impact(self):
         self._skipMissingKey("set_bus")

grid2op/tests/test_Observation.py
@@ -261,14 +261,14 @@ class TestBasisObsBehaviour(unittest.TestCase):
             "gen_min_downtime": [96, 4, 0, 0, 4],
             "gen_cost_per_MW": [5.0, 10.0, 0.0, 0.0, 10.0],
             "gen_startup_cost": [20.0, 2.0, 0.0, 0.0, 2.0],
-            "gen_shutdown_cost": [0.0] * 5,
-            "load_size":[0.0]*11,
-            "load_flexible":[False]*11,
-            "load_max_ramp_up": [0.0]*11,
-            "load_max_ramp_down": [0.0]*11,
-            "load_min_uptime": [0.0]*11,
-            "load_min_downtime": [0.0]*11,
-            "load_cost_per_MW": [0.0]*11,
+            "gen_shutdown_cost": [10.0, 1.0, 0.0, 0.0, 1.0],
+            "load_size":None,#[0.0]*11,
+            "load_flexible":None,#[False]*11,
+            "load_max_ramp_up":None,#[0.0]*11,
+            "load_max_ramp_down":None,#[0.0]*11,
+            "load_min_uptime":None,#[0.0]*11,
+            "load_min_downtime":None,#[0.0]*11,
+            "load_cost_per_MW":None,#[0.0]*11,
             "grid_layout": {
                 "sub_0": [-280.0, -81.0],
                 "sub_1": [-100.0, -270.0],
@@ -738,6 +738,8 @@ class TestBasisObsBehaviour(unittest.TestCase):
             ],
             "target_dispatch": [0.0, 0.0, 0.0, 0.0, 0.0],
             "actual_dispatch": [0.0, 0.0, 0.0, 0.0, 0.0],
+            "target_flex": [0.0]*11,
+            "actual_flex": [0.0]*11,
             "_shunt_p": [0.0],
             "_shunt_q": [-17.923625946044922],
             "_shunt_v": [0.20202238857746124],
@@ -885,8 +887,13 @@ class TestBasisObsBehaviour(unittest.TestCase):
                 dt_int,
                 dt_int,
                 dt_int,
+                # Redispatch
                 dt_float,
                 dt_float,
+                # Flexibility
+                dt_float,
+                dt_float,
+                # Storage
                 dt_float,
                 dt_float,
                 dt_float,
@@ -958,8 +965,12 @@ class TestBasisObsBehaviour(unittest.TestCase):
                 14,
                 20,
                 20,
+                # Redispatch
                 5,
                 5,
+                # Flexibility
+                11,
+                11,
                 0,
                 0,
                 0,
@@ -992,7 +1003,7 @@ class TestBasisObsBehaviour(unittest.TestCase):
                 0
             ]
         )
-        self.size_obs = 429 + 4 + 4 + 2 + 1 + 10 + 5 + 0
+        self.size_obs = 429 + 4 + 4 + 2 + 1 + 10 + 5 + 0 + 11 + 11
 
     def tearDown(self):
         self.env.close()
@@ -2064,24 +2075,25 @@ class TestBasisObsBehaviour(unittest.TestCase):
         assert np.all(shapes == self.shapes)
 
     def test_4_to_from_vect(self):
-        # test that helper_obs is able to generate a valid observation
-        obs = self.env.observation_space(self.env)
-        obs2 = self.env.observation_space(self.env)
-        vect = obs.to_vect()
-        assert vect.shape[0] == obs.size()
-        obs2.reset()
-        obs2.from_vect(vect)
-        assert np.all(obs.dtypes() == self.dtypes)
-        assert np.all(obs.shapes() == self.shapes)
+        with warnings.catch_warnings(category=DeprecationWarning, action="ignore"):
+            # test that helper_obs is able to generate a valid observation
+            obs = self.env.observation_space(self.env)
+            obs2 = self.env.observation_space(self.env)
+            vect = obs.to_vect()
+            assert vect.shape[0] == obs.size()
+            obs2.reset()
+            obs2.from_vect(vect)
+            assert np.all(obs.dtypes() == self.dtypes)
+            assert np.all(obs.shapes() == self.shapes)
 
-        # TODO there is not reason that these 2 are equal: reset, will erase everything
-        # TODO whereas creating the observation
-        # assert obs == obs2
-        obs_diff, attr_diff = obs.where_different(obs2)
-        for el in attr_diff:
-            assert el in obs.attr_list_json, f"{el} should be equal in obs and obs2"
-        vect2 = obs2.to_vect()
-        assert np.all(vect == vect2)
+            # TODO: There is no reason that these 2 are equal: reset, will erase everything
+            # TODO: whereas creating the observation
+            # assert obs == obs2
+            obs_diff, attr_diff = obs.where_different(obs2)
+            for el in attr_diff:
+                assert el in obs.attr_list_json, f"{el} should be equal in obs and obs2"
+            vect2 = obs2.to_vect()
+            assert np.all(vect == vect2)
 
     def test_5_simulate_proper_timestep(self):
         self.skipTest(
@@ -2197,29 +2209,30 @@ class TestBasisObsBehaviour(unittest.TestCase):
         assert dict_["nb_elements"] == 6
 
     def test_space_to_dict(self):
-        dict_ = self.env.observation_space.cls_to_dict()
-        for el in dict_:
-            assert el in self.dict_, f"missing key {el} in self.dict_"
-        for el in self.dict_:
-            assert el in dict_, f"missing key {el} in dict_"
-            
-        for el in self.dict_:
-            val = dict_[el]
-            val_res = self.dict_[el]
-            if val is None and val_res is not None:
-                raise AssertionError(f"{el}: val is None and val_res is not None: val_res: {val_res}")
-            if val is not None and val_res is None:
-                raise AssertionError(f"{el}: val is not None and val_res is None: val {val}")
-            if val is None and val_res is None:
-                continue
-            
-            ok_ = np.array_equal(val, val_res)
-            assert ok_, (f"values different for {el}: "
-                         f"{dict_[el]} vs "
-                         f"{self.dict_[el]}")
-            
-        # self.maxDiff = None
-        # self.assertDictEqual(dict_, self.dict_)
+        with warnings.catch_warnings(category=DeprecationWarning, action="ignore"):
+            dict_ = self.env.observation_space.cls_to_dict()
+            for el in dict_:
+                assert el in self.dict_, f"missing key {el} in self.dict_"
+            for el in self.dict_:
+                assert el in dict_, f"missing key {el} in dict_"
+                
+            for el in self.dict_:
+                val = dict_[el]
+                val_res = self.dict_[el]
+                if val is None and val_res is not None:
+                    raise AssertionError(f"{el}: val is None and val_res is not None: val_res: {val_res}")
+                if val is not None and val_res is None:
+                    raise AssertionError(f"{el}: val is not None and val_res is None: val {val}")
+                if val is None and val_res is None:
+                    continue
+                
+                ok_ = np.array_equal(val, val_res)
+                assert ok_, (f"values different for {el}: "
+                            f"{dict_[el]} vs "
+                            f"{self.dict_[el]}")
+                
+            # self.maxDiff = None
+            # self.assertDictEqual(dict_, self.dict_)
 
     def test_from_dict(self):
         res = ObservationSpace.from_dict(self.dict_)
@@ -2310,31 +2323,32 @@ class TestBasisObsBehaviour(unittest.TestCase):
 
     def test_to_from_json(self):
         """test the to_json, and from_json and make sure these are all  json serializable"""
-        obs = self.env.observation_space(self.env)
-        obs2 = self.env.observation_space(self.env)
-        dict_ = obs.to_json()
+        with warnings.catch_warnings(category=DeprecationWarning, action="ignore"):
+            obs = self.env.observation_space(self.env)
+            obs2 = self.env.observation_space(self.env)
+            dict_ = obs.to_json()
 
-        # test that the right dictionary is returned
-        for k in dict_:
-            assert (
-                dict_[k] == self.json_ref[k]
-            ), f"error for key {k} (in dict_): {dict_[k]} vs {self.json_ref[k]} "
-        for k in self.json_ref:
-            assert dict_[k] == self.json_ref[k], f"error for key {k} (in self.json_ref)"
-        self.assertDictEqual(dict_, self.json_ref)
+            # test that the right dictionary is returned
+            for k in dict_:
+                assert (
+                    dict_[k] == self.json_ref[k]
+                ), f"error for key {k} (in dict_): {dict_[k]} vs {self.json_ref[k]} "
+            for k in self.json_ref:
+                assert dict_[k] == self.json_ref[k], f"error for key {k} (in self.json_ref)"
+            self.assertDictEqual(dict_, self.json_ref)
 
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            # test i can save it (json serializable)
-            with open(os.path.join(tmpdirname, "test.json"), "w") as fp:
-                json.dump(obj=dict_, fp=fp)
-            # test i can properly load it back
-            with open(os.path.join(tmpdirname, "test.json"), "r") as fp:
-                dict_realoaded = json.load(fp=fp)
-        assert dict_realoaded == dict_
-        # test i can initialize an observation from it
-        obs2.reset()
-        obs2.from_json(dict_realoaded)
-        assert obs == obs2
+            with tempfile.TemporaryDirectory() as tmpdirname:
+                # test i can save it (json serializable)
+                with open(os.path.join(tmpdirname, "test.json"), "w") as fp:
+                    json.dump(obj=dict_, fp=fp)
+                # test i can properly load it back
+                with open(os.path.join(tmpdirname, "test.json"), "r") as fp:
+                    dict_realoaded = json.load(fp=fp)
+            assert dict_realoaded == dict_
+            # test i can initialize an observation from it
+            obs2.reset()
+            obs2.from_json(dict_realoaded)
+            assert obs == obs2
 
 
 class TestUpdateEnvironement(unittest.TestCase):
@@ -2578,21 +2592,22 @@ class TestSimulateEqualsStep(unittest.TestCase):
             raise AssertionError(f"Following attributes are different: {attr_diff}")
 
     def test_redispatch(self):
-        if DEACTIVATE_FAILING_TEST:
-            return
-        # Find first redispatchable generator
-        gen_id = next((i for i, j in enumerate(self.obs.gen_redispatchable) if j), None)
-        # Create valid ramp up
-        redisp_val = self.obs.gen_max_ramp_up[gen_id] / 2.0
-        # Create redispatch action
-        redisp_act = self.env.action_space({"redispatch": [(gen_id, redisp_val)]})
-        # Simulate & Step
-        self.sim_obs, _, _, _ = self.obs.simulate(redisp_act)
-        self.step_obs, _, _, _ = self.env.step(redisp_act)
-        # Test observations are the same
-        if self.sim_obs != self.step_obs:
-            diff_, attr_diff = self.sim_obs.where_different(self.step_obs)
-            raise AssertionError(f"Following attributes are different: {attr_diff}")
+        with warnings.catch_warnings(category=DeprecationWarning, action="ignore"):
+            if DEACTIVATE_FAILING_TEST:
+                return
+            # Find first redispatchable generator
+            gen_id = next((i for i, j in enumerate(self.obs.gen_redispatchable) if j), None)
+            # Create valid ramp up
+            redisp_val = self.obs.gen_max_ramp_up[gen_id] / 2.0
+            # Create redispatch action
+            redisp_act = self.env.action_space({"redispatch": [(gen_id, redisp_val)]})
+            # Simulate & Step
+            self.sim_obs, _, _, _ = self.obs.simulate(redisp_act)
+            self.step_obs, _, _, _ = self.env.step(redisp_act)
+            # Test observations are the same
+            if self.sim_obs != self.step_obs:
+                diff_, attr_diff = self.sim_obs.where_different(self.step_obs)
+                raise AssertionError(f"Following attributes are different: {attr_diff}")
 
     def test_change_simulate_reward(self):
         """test the env.observation_space.change_other_reward function"""
@@ -2913,26 +2928,27 @@ class TestSimulateEqualsStep(unittest.TestCase):
         ), "issue with storage_power"
 
     def test_simulate_current_ts(self):
-        sim_obs, _, _, _ = self.obs.simulate(self.env.action_space(), time_step=0)
-        # check that the observations are equal
-        self._check_equal(sim_obs, self.obs)
+        with warnings.catch_warnings(category=DeprecationWarning, action="ignore"):
+            sim_obs, _, _, _ = self.obs.simulate(self.env.action_space(), time_step=0)
+            # check that the observations are equal
+            self._check_equal(sim_obs, self.obs)
 
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore")
-            obs = self.env.reset()
-        sim_obs1, rew1, done1, _ = obs.simulate(
-            self.env.action_space.disconnect_powerline(line_id=2)
-        )
-        sim_obs2, rew2, done2, _ = obs.simulate(self.env.action_space(), time_step=0)
-        sim_obs3, rew3, done3, _ = obs.simulate(
-            self.env.action_space.disconnect_powerline(line_id=2)
-        )
-        assert not done1
-        assert not done2
-        assert not done3
-        self._check_equal(sim_obs2, obs)
-        assert abs(rew1 - rew3) <= 1e-8, "issue with reward"
-        self._check_equal(sim_obs1, sim_obs3)
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore")
+                obs = self.env.reset()
+            sim_obs1, rew1, done1, _ = obs.simulate(
+                self.env.action_space.disconnect_powerline(line_id=2)
+            )
+            sim_obs2, rew2, done2, _ = obs.simulate(self.env.action_space(), time_step=0)
+            sim_obs3, rew3, done3, _ = obs.simulate(
+                self.env.action_space.disconnect_powerline(line_id=2)
+            )
+            assert not done1
+            assert not done2
+            assert not done3
+            self._check_equal(sim_obs2, obs)
+            assert abs(rew1 - rew3) <= 1e-8, "issue with reward"
+            self._check_equal(sim_obs1, sim_obs3)
 
     def test_nb_step(self):
         with warnings.catch_warnings():